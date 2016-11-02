245ffedc64f4ab559e4cf23fdacaa43f392c731d
sqiou11 <sqiou11@gmail.com>
2016-11-02 09:38:39 -0500

    Multiple threads used for parallel processing
    Now using a thread pool to process and scrape all of the apartment links
    in a parallel fashion

--- scrapers/rolandRealty.py
+++ scrapers/rolandRealty.py
@@ -1,38 +1,30 @@
 from bs4 import BeautifulSoup
-import re, psycopg2, os
 from selenium import webdriver
+import re, psycopg2, os, time, multiprocessing, multiprocessing.dummy, socket, errno
 
 db_con = psycopg2.connect(database='aptFinderDB', user=os.environ['DB_USER'], password=os.environ['DB_PASS'])
 cursor = db_con.cursor()
 
 domain = "http://www.roland-realty.com"
-urls = [
-    "http://www.roland-realty.com/studio-apartments.html",
-    #"http://www.roland-realty.com/1-bedroom-apartments.html",
-    #"http://www.roland-realty.com/2-bedroom-apartments.html",
-    #"http://www.roland-realty.com/3-bedroom-apartments.html",
-    #"http://www.roland-realty.com/4-bedroom-apartments.html",
-    #"http://www.roland-realty.com/5-bedroom-apartments.html"
+sources = [
+    { "type": "studio", "url": "http://www.roland-realty.com/studio-apartments.html" },
+    { "type": "1 bed", "url": "http://www.roland-realty.com/1-bedroom-apartments.html" },
+    { "type": "2 bed", "url": "http://www.roland-realty.com/2-bedroom-apartments.html" },
+    { "type": "3 bed", "url": "http://www.roland-realty.com/3-bedroom-apartments.html" },
+    { "type": "4 bed", "url": "http://www.roland-realty.com/4-bedroom-apartments.html" },
+    { "type": "5 bed", "url": "http://www.roland-realty.com/5-bedroom-apartments.html" }
 ]
 
+def getAptData(aptTuple):
+    aptType = aptTuple[0]
+    url = aptTuple[1]
 
-for url in urls:
-    data = []
-    driver = webdriver.PhantomJS()
-    driver.get(url)
-
-    soup = BeautifulSoup(driver.page_source, "lxml")
-    table = soup.find("table", class_="wsite-multicol-table")
-    apartments = table.find_all(class_="wslide-slide")
-
-    for apt in apartments:
-        aptUrl = domain + apt.find("a")['href']
-
-        # start an apartment-specific PhantomJS instance to grab details about individual apartments
+    print("Starting thread for " + str(aptTuple))
+    try:
         aptDriver = webdriver.PhantomJS()
-        aptDriver.get(aptUrl)
+        aptDriver.get(url)
         aptSoup = BeautifulSoup(aptDriver.page_source, "lxml")
-        aptName = aptSoup.find(class_="wsite-content-title").string
+        aptName = aptSoup.find(class_="wsite-content-title").string.replace(u'\xa0', ' ')
         aptMapSource = aptSoup.find(class_="wsite-map").find('iframe')['src']
 
         # grab longitude and latitude coordinates for this apartment
@@ -41,16 +33,50 @@ for url in urls:
         latitude = longLatMatch.group(2)
 
         aptDriver.quit()
+        #threadLock.acquire()
+        data.append({'name': aptName, 'url': url, 'longitude': longitude, 'latitude': latitude, 'type': aptType})
+        #threadLock.release()
+        print("Exiting thread for " + url)
+    except socket.error as error:
+        if error.errno == errno.WSAECONNRESET:
+            print("Socket connection closed unexpectedly for " + url + "! Retrying...")
+            getAptData(aptTuple)
 
-        data.append({'name': aptName, 'url': aptUrl, 'longitude': longitude, 'latitude': latitude})
+# parallel processing of all urls using a pool of threads
+def processUrls(aptUrls, threads=2):
+    pool = multiprocessing.dummy.Pool(threads)
+    pool.map(getAptData, aptUrls)
+    pool.close()
+    pool.join()
 
+start = time.clock()
+data = []
+aptUrls = []
+threadLock = multiprocessing.Lock()
+
+for source in sources:
+    driver = webdriver.PhantomJS()
+    driver.get(source['url'])
+
+    soup = BeautifulSoup(driver.page_source, "lxml")
+    table = soup.find("table", class_="wsite-multicol-table")
+    apartments = table.find_all(class_="wslide-slide")
+
+    for apt in apartments:
+        aptUrl = apt.find("a")['href']
+        if aptUrl != '/':
+            aptUrls.append((source['type'], domain + aptUrl))
     driver.quit()
+    print("Finished collecting urls from " + source['url'])
 
-    for aptData in data:
-        print(aptData)
-        cursor.execute("""
-            INSERT INTO public.apartments (company, name, url, longitude, latitude) VALUES (%s, %s, %s, %s, %s)
-            ON CONFLICT (company, name) DO UPDATE SET (url, longitude, latitude) = (%s, %s, %s)
-            """, ("Roland Realty", aptData['name'], aptData['url'], aptData['longitude'], aptData['latitude'], aptData['url'], aptData['longitude'], aptData['latitude']))
-        db_con.commit()
-    db_con.close()
+processUrls(aptUrls, 16)
+
+for aptData in data:
+    print(aptData)
+    cursor.execute("""
+        INSERT INTO public.apartments (company, name, url, longitude, latitude, type) VALUES (%s, %s, %s, %s, %s, %s)
+        ON CONFLICT (company, name, type) DO NOTHING
+        """, ("Roland Realty", aptData['name'], aptData['url'], aptData['longitude'], aptData['latitude'], aptData['type']))
+    db_con.commit()
+db_con.close()
+print(time.clock() - start)

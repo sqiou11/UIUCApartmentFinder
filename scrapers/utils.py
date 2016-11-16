fc65f213a986861270948dc09feaa69eef6eeaea
sqiou11 <sqiou11@gmail.com>
2016-11-16 14:37:40 -0600

    added more files with helper functions

--- scrapers/utils.py
+++ scrapers/utils.py
@@ -1,7 +1,8 @@
-import multiprocessing
-
-# call processFunc on every member of the dataArray using a thread pool
+"""
+call processFunc on every member of the dataArray using a thread pool
+"""
 def executeParallel(processFunc, dataArray, poolSize=4):
+    import multiprocessing
     pool = multiprocessing.Pool(poolSize)
     results = pool.map(processFunc, dataArray)
     pool.close()
